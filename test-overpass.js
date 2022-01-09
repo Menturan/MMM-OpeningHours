7d9a7e8f4d4d613324c8ae8869b67a7fba568055
Arthur Lutz <arthur@lutz.im>
2022-01-10 00:23:39 +0100

    test scripts - small fixes

--- test-overpass.js
+++ test-overpass.js
@@ -7,13 +7,13 @@ console.log('openstreetmap: {')
 console.log('   places: [')
 overpassFrontend.BBoxQuery(
   'nwr[opening_hours]',
+  // TODO - which location should be used for the example ?
   {  minlat: 47.252562658480834, minlon:-1.5286446541310434, maxlat:47.257594550572655,maxlon:-1.5083456963063904},
 
   {
     properties: OverpassFrontend.ALL
   },
   function (err, result) {
-    ["xxxxxxxx", ["yyyyyyyy", "Alias y"]]
     console.log('["' + result.id + '", "' + result.tags.name + '"],')
   },
   function (err) {
